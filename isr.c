22da0903d9becc9a45682e95edecbbcd1f49abdc
Emil Peñaló <emil@Emils-MacBook-Pro.local>
2020-10-25 17:14:07 -0400

    +Definición meses +Función salida +Ingreso y Validación de bruto

---
+++ isr.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LEN 11
+#define CANT 12
+
+void salida(char [][LEN], float [], float [], float []);
+
+int main() {
+   // Definicion del arreglo de los meses
+   char meses[CANT][LEN] = { {'E','n','e','r','o'},
+                           {'F','e','b','r','e','r','o'},
+                           {'M','a','r','z','o'},
+                           {'A','b','r','i','l'},
+                           {'M','a','y','o'},
+                           {'J','u','n','i','o'},
+                           {'J','u','l','i','o'},
+                           {'A','g','o','s','t','o'},
+                           {'S','e','p','t','i','e','m','b','r','e'},
+                           {'O','c','t','u','b','r','e'},
+                           {'N','o','v','i','e','m','b','r','e'},
+                           {'D','i','c','i','e','m','b','r','e'} };
+
+   float bruto[CANT];
+
+   for (int i = 0; i < CANT; i++) {
+      do {
+         printf("Ingrese el sueldo bruto de %s:\n", meses[i]);
+         scanf("%f\n", &bruto[i]);
+
+         if (bruto[i] <= 0) {
+            printf("Ingrese un numero mayor que cero\n");
+         }
+
+      } while(bruto[i] <= 0);
+   }
+
+   float isr[CANT] = {12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12};
+   float neto[CANT] = {12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12};
+   salida(meses, bruto, isr, neto);
+
+   return 0;
+}
+
+/*
+   Funcion: salida
+   Argumentos: (char[]) meses. Un arreglo con todos los meses del año
+               (float[]) bruto. Arreglo con el sueldo bruto de cada mes
+               (float[]) isr. Arreglo con el ISR de cada mes
+               (float[]) neto. Arreglo con el sueldo neto de cada mes
+   Objetivo: Imprimir los datos en forma de tabla a la consola.
+   Retorno: void
+*/
+void salida(char meses[][LEN], float bruto[], float isr[], float neto[]) {\
+
+   // Salida Titulos
+   printf("Meses\t\t");
+   printf("Sueldo Bruto\t");
+   printf("ISR\t\t");
+   printf("Sueldo Neto\n");
+
+   // Salida datos por mes
+   for (int i = 0; i < CANT; i++) {
+      printf("%-10s:\t", meses[i]);
+      printf("%-10f\t", bruto[i]);
+      printf("%-10f\t", isr[i]);
+      printf("%-10f\n", neto[i]);
+   }
+}
